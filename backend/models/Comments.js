50cfbcc72bf59d385294dc872ddf17f87a189fbe
Muhammed-Ali-Ktr <muhammedalikitir.tr@gmail.com>
2025-08-31 22:46:22 +0300

    Delete backend directory

--- backend/models/Comments.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const CommentSchema = new mongoose.Schema({
-  text: String,
-  user: { type: String, default: "Anonim" },
-  date: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("Comment", CommentSchema);
